76c7b88f8c1be1cdb602ca9a04bb2a93e22eaad6
digonzaga1987 <digonzaga-ata@hotmail.com>
2015-09-26 11:41:16 -0300

    Commit parcial

--- climaAgora/www/js/app.js
+++ climaAgora/www/js/app.js
@@ -18,8 +18,58 @@ climaApp.run(function($ionicPlatform) {
   });
 })
 
-climaApp.controller("climaCtrl",["$scope","$ionicLoading",climaCtrl]);
+climaApp.controller("climaCtrl",["$scope","$ionicLoading","climaSvc",climaCtrl]);
 
-function climaCtrl($scope,$ionicLoading){
-    $scope.cidade = "Lins";
-}
+function climaCtrl($scope,$ionicLoading,climaSvc){
+    $scope.cidade           = "Lins";
+    $scope.temperatura      = "ND";
+    $scope.temp_min         = "ND";
+    $scope.temp_max         = "ND";
+    $scope.latitude         = "ND";
+    $scope.longitude        = "ND";
+    $scope.pressure         = "ND";
+    $scope.humidity         = "ND";   
+    $scope.temp_description = "ND";
+    $scope.temp_icon        = "ND";
+    
+    climaSvc.loadClima();
+    
+    
+    $scope.$on("climaApp.temperatura",function(_,result){
+        $scope.temperatura = result.main.temp;
+        $scope.temp_min    = result.main.temp_min;
+        $scope.temp_max    = result.main.temp_max;          
+        $scope.latitude    = result.coord.lat;    
+        $scope.longitude   = result.coord.lon;            
+        $scope.pressure    = result.main.pressure;
+        $scope.humidity    = result.main.humidity; 
+        $scope.temp_icon   = "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png";
+        $scope.temp_description = result.weather[0].description;
+        
+        
+        
+    }
+              
+              );// fim do climaApp.temperatura
+    
+    
+}// fim do contrroler
+
+climaApp.service("climaSvc",["$http","$rootScope",climaSvc]);
+function climaSvc($http,$rootScope){
+    this.loadClima = function(){
+        console.log("Carregando Clima");
+        url = "http://api.openweathermap.org/data/2.5/weather?lat=-21.6692&lon=-49.6933&units=metric&lang=pt";
+        $http.get(url,{params :""}).success(function(result){
+            console.log("Temperatura Carregada");
+            $rootScope.$broadcast("climaApp.temperatura",result);
+            
+        
+        }).error(function(result){
+            console.log("Erro ao Carregar Temperatura");
+        
+        });
+    }
+    
+}// fim do service
+
